56c8ce4a85e61c3b14d018509c0e5169fd008264
Dizziee <66963438+AverageFarmer@users.noreply.github.com>
2022-12-21 19:24:35 -0500

    asd

---
+++ contact.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<style>
+    /* Add some basic styling */
+    body {
+        font-family: sans-serif;
+        max-width: 800px;
+        margin: 0 auto;
+        padding: 20px;
+    }
+    
+    h1, h2, h3 {
+        text-align: center;
+    }
+    
+    /* Style the navigation menu */
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #333;
+        color: #fff;
+        padding: 10px 20px;
+    }
+    
+    nav ul {
+        display: flex;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+    
+    nav li {
+        margin: 0 10px;
+    }
+    
+    nav a {
+        color: #fff;
+        text-decoration: none;
+    }
+    
+    /* Style the video embeds */
+    iframe {
+        display: block;
+        margin: 20px auto;
+        border: 1px solid #ccc;
+    }
+</style>
+<nav>
+    <h1>My Portfolio</h1>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="aboutme.php">About Me</a></li>
+        <li><a href="work.php">My Work</a></li>
+        <!--<li><a href="contact.php">Contact Me</a></li>-->
+    </ul>
+</nav>
+<head>
+	<title>About Me</title>
+</head>
+<body>
+	<h1>About Me</h1>
+	<p>Hello! I am a creative professional with a passion for creating engaging and impactful media. I specialize in video production and have a strong background in design and storytelling. Take a look at my work to see some of my past projects.</p>
+</body>
+</html>
